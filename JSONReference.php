8ada6b0c62bdc9d26de555cf9c96b4eabe35e4a7
Steffen Mächtel <info@steffen-maechtel.de>
2015-08-23 19:34:34 +0200

    Implemented encode php function

--- JSONReference.php
+++ JSONReference.php
@@ -7,12 +7,40 @@
  */
 class JSONReference {
 
-    public static function encode($value) {
+    public static function encode($data) {
+        $objects = new stdClass();
+
+        $root = self::extractObjectsAndReplaceWithHashRecursive($data, $objects);
+
+        return (object) array('root' => $root, 'objects' => $objects);
+    }
+
+    public static function decode($data) {
         
     }
 
-    public static function decode($value) {
-        
+    protected static function extractObjectsAndReplaceWithHashRecursive($data, $objects, & $reached = array()) {
+        switch (true) {
+            case is_array($data):
+                foreach ($data as $key => $value) {
+                    $data[$key] = self::extractObjectsAndReplaceWithHashRecursive($value, $objects, $reached);
+                }
+                return $data;
+            case is_object($data):
+                $hash = '@' . spl_object_hash($data);
+                if (in_array($hash, $reached) === false) {
+                    $reached[] = $hash;
+                    $object = new stdClass();
+                    foreach ($data as $property => $value) {
+                        $object->$property = self::extractObjectsAndReplaceWithHashRecursive($value, $objects, $reached);
+                    }
+                    $objects->$hash = $object;
+                }
+                return $hash;
+
+            default:
+                return $data;
+        }
     }
 
 }
