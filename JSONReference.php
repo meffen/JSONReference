7669f698d345ac0819ab6600c62d85131cd33add
Steffen Mächtel <info@steffen-maechtel.de>
2015-08-23 22:43:23 +0200

    Implemented decode php function

--- JSONReference.php
+++ JSONReference.php
@@ -7,18 +7,49 @@
  */
 class JSONReference {
 
+    /**
+     * @param mixed $data
+     * @return \stdClass {root: ..., objects: ...}
+     */
     public static function encode($data) {
-        $objects = new stdClass();
+        $objects = new \stdClass();
 
         $root = self::extractObjectsAndReplaceWithHashRecursive($data, $objects);
 
         return (object) array('root' => $root, 'objects' => $objects);
     }
 
+    /**
+     * @param \stdClass $data
+     * @return mixed
+     * @throws \InvalidArgumentException
+     */
     public static function decode($data) {
-        
+        if (($data instanceof \stdClass) === false) {
+            throw new \InvalidArgumentException('Parameter data is not an instance of stdClass');
+        }
+
+        if (property_exists($data, 'root') === false) {
+            throw new \InvalidArgumentException('Parameter data has no property root');
+        }
+
+        if (property_exists($data, 'objects') === false) {
+            throw new \InvalidArgumentException('Parameter data has no property objects');
+        }
+
+        if (is_array($data->objects) && count($data->objects) > 0) {
+            throw new \InvalidArgumentException('Parameter data->objects has items, but is not an instance of stdClass');
+        }
+
+        return self::replace_hash_with_objects($data->root, clone $data->objects);
     }
 
+    /**
+     * @param mixed $data
+     * @param \stdClass $objects
+     * @param array $reached
+     * @return string
+     */
     protected static function extractObjectsAndReplaceWithHashRecursive($data, $objects, & $reached = array()) {
         switch (true) {
             case is_array($data):
@@ -38,6 +69,37 @@ class JSONReference {
                 }
                 return $hash;
 
+            default:
+                return $data;
+        }
+    }
+
+    /**
+     * 
+     * @param mixed $data
+     * @param \stdClass $objects
+     * @param array $reached
+     * @return mixed
+     * @throws \Exception
+     */
+    protected static function replaceHashWithObjectsRecursive($data, $objects, & $reached = array()) {
+        switch (true) {
+            case is_array($data):
+                foreach ($data as $key => $value) {
+                    $data[$key] = self::replaceHashWithObjectsRecursive($value, $objects, $reached);
+                }
+                return $data;
+            case is_string($data) === true && $data[0] === '@':
+                if (property_exists($objects, $data) === false) {
+                    throw new \Exception('Object with hash "' . $data . '" does not exists in objects');
+                }
+                if (in_array($data, $reached) === false) {
+                    $reached[] = $data;
+                    foreach ($objects->$data as $property => $value) {
+                        $objects->$data->$property = self::replaceHashWithObjectsRecursive($value, $objects, $reached);
+                    }
+                }
+                return $objects->$data;
             default:
                 return $data;
         }
